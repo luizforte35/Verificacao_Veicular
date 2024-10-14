6800fe27c865e121204d43f41f0ba5b2b75483f8
luizf <luizforte35@gmail.com>
2024-10-13 22:06:15 -0300

    First

---
+++ app/src/main/java/com/example/verificacao_veicular/Layout/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.verificacao_veicular.Layout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.verificacao_veicular.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button iniciarVerificacaoButton;
+    private Button historicoButton;
+    private Button configuracoesButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+            iniciarVerificacaoButton = findViewById(R.id.iniciarVerificacaoButton);
+            historicoButton = findViewById(R.id.historicoButton);
+            configuracoesButton = findViewById(R.id.configuracoesButton);
+
+            iniciarVerificacaoButton.setOnClickListener(v -> {
+                Intent intent = new Intent(MainActivity.this, Verificacao.class);
+                startActivity(intent);
+            });
+
+
+
+
+
+    }//fim do onCreate
+}//class
